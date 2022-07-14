f3a7311c6442f052b874e27c0a35baf066257a40
stantoha <78742661+stantoha@users.noreply.github.com>
2022-07-14 17:37:03 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,56 +3,144 @@ window.addEventListener('DOMContentLoaded', () => {
 
     let 
         header = document.querySelector('.header'),
-        tabParent = document.querySelector('.header__nav__menu'),
+        title=document.querySelector('.header__title'),
+        navMenu = document.querySelector('.header__nav__menu'),
         navMenuButton = document.getElementById('nav__toggle'),
-        navButton=document.querySelector('.nav__btn');
+        navButton=document.querySelector('.nav__btn'),
+        tabs = document.querySelectorAll('.header__nav__menu__link'),
+        tabsContent=document.querySelectorAll('.section__tabcontent');
 
-    function showNavMenu(){
-            tabParent.classList.remove('header__nav__menu__hidden');
-            tabParent.classList.add('header__nav__menu__visible'); 
+        function showNavMenu(){
+            navMenu.classList.remove('header__nav__menu');
+            navMenu.classList.add('header__nav__menu__visible');
                 navButton.classList.add('nav__btn__transform');
             
     }
 
     function hideNavMenu(){
-        tabParent.classList.add('header__nav__menu__hidden');
-        tabParent.classList.remove('header__nav__menu__visible'); 
+        navMenu.classList.add('header__nav__menu');
+        navMenu.classList.remove('header__nav__menu__visible'); 
         navButton.classList.remove('nav__btn__transform');
     }
 
-    if(document.documentElement.clientWidth<991.98){
-        navButton.classList.remove('hide');
-        navButton.classList.add('show','fade');
-        tabParent.classList.remove('header__nav__menu');
-        tabParent.classList.add('header__nav__menu__hidden');
-        if(!navMenuButton.classList.contains('nav__btn__transform')){
-            tabParent.classList.add('header__nav__menu__hidden');
+    navMenuButton.addEventListener('click',()=>{
+        if(navButton.classList.contains('nav__btn__transform')){
+            hideNavMenu();
         }
         else{
-            tabParent.classList.add('header__nav__menu__visible');
-            tabParent.classList.remove('header__nav__menu__hidden');
-        }
-            }
-            else{
-                navButton.classList.add('hide');
-                navButton.classList.remove('show','fade');
-                tabParent.classList.remove('header__nav__menu__visible');
-            tabParent.classList.remove('header__nav__menu__hidden');
-            tabParent.classList.add('header__nav__menu');
-            }
-
-    navMenuButton.addEventListener('click',()=>{
-        if(navMenuButton.checked){
             showNavMenu();
         }
-       else {
-                hideNavMenu();
-            }
     });
-        
-   
 
-       
+
+    
+  /*   upButton.classList.add('hide'); */
+
+    setTimeout(() => {
+        title.classList.remove('top__slide');
+       /*  subTitle.classList.remove('hide');
+        subTitle.classList.add('show', 'fade'); */
+    }, 1000);
+
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+        tabs.forEach(item => {
+            item.classList.remove('active');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('active');
+
+       /*  if (sectionTitles[i].classList.contains('right__slide')) {
+            setTimeout(() => {
+                sectionTitles[i].classList.remove('right__slide');
+            }, 1000);
+        } else if (sectionTitles[i].classList.contains('left__slide')) {
+            setTimeout(() => {
+                sectionTitles[i].classList.remove('left__slide');
+            }, 1000);
+        } */
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    /* function hideServiceTabContent() {
+        servicesTabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+        
+        serviceTabs.forEach(item => {
+            item.parentElement.classList.remove('active__service');
+        });
+    }
+
+    function showServiceTabContent(j = 0) {
+        servicesTabsContent[j].classList.add('show', 'fade');
+        servicesTabsContent[j].classList.remove('hide');
+        serviceTabs[j].parentElement.classList.add('active__service');
+    }
+
+    hideServiceTabContent(); */
+
+    navMenu.addEventListener('click', (event) => {
+        let target = event.target;
+        if (target && target.classList.contains('header__nav__menu__link')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    window.scrollTo(0, 0);
+                    hideTabContent();
+                    showTabContent(i);
+                    if(document.documentElement.clientWidth< 900){
+                        hideNavMenu();
+                    }
+                    
+                    /* if (i == 1) {
+                        setTimeout(() => {
+                            serviceTitles[0].classList.remove('right__slide');
+                        }, 1000);
+                        showServiceTabContent();
+                     
+                        servicesnavMenu.addEventListener('click', (event) => {
+                            let target = event.target;
+                            if (target && target.classList.contains('service__button')) {
+                                
+                                serviceTabs.forEach((item, j) => {
+                                    if (target == item) {
+                                        hideServiceTabContent();
+                                        showServiceTabContent(j);
+
+                                        serviceTitles.forEach(item => {
+                                            if (serviceTitles[j].classList.contains('right__slide')) {
+                                                setTimeout(() => {
+                                                    serviceTitles[j].classList.remove('right__slide');
+                                                }, 1000);
+                                            } else if (serviceTitles[j].classList.contains('left__slide')) {
+                                                setTimeout(() => {
+                                                    serviceTitles[j].classList.remove('left__slide');
+                                                }, 1000);
+                                            }
+                                        });
+
+                                    }
+                                });
+                            }
+                        });
+                    } */
+                }
+            });
+        }
+    });
+
+
 
 
 
