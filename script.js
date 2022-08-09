45248a78d505eb7d3d0f0866b8dcd37276677ee9
stantoha <78742661+stantoha@users.noreply.github.com>
2022-08-09 19:05:53 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,203 +1,246 @@
-window.addEventListener('DOMContentLoaded', () => {
-    //SHOW-HIDE CONTENT
+window.addEventListener("DOMContentLoaded", () => {
+  //SHOW-HIDE CONTENT
 
-    let 
-        header = document.querySelector('.header'),
-        title=document.querySelector('.header__title'),
-        navMenu = document.querySelector('.header__nav__menu'),
-        navMenuButton = document.getElementById('nav__toggle'),
-        navButton=document.querySelector('.nav__btn'),
-        tabs = document.querySelectorAll('.header__nav__menu__link'),
-        tabsContent=document.querySelectorAll('.section'),
-        serviceTabs = document.querySelectorAll('.service__button'),
-        servicesTabsContent = document.querySelectorAll('.service'),
-        servicesTabParent = document.querySelector('.services__list'),
-        upButton = document.querySelector('.up__button'),
-        sectionTitles = document.querySelectorAll('.section__title'),
-        serviceTitles = document.querySelectorAll('.service__title'),
-        subTitle = document.querySelector('.sub__title'),
-        modal = document.querySelector('.modal__container'),
-        modalTrigger = document.querySelectorAll('[data-modal]'),
-        modalClosebtn = document.querySelector('[data-close]');
+  let header = document.querySelector(".header"),
+    title = document.querySelector(".header__title"),
+    sectionTitles = document.querySelectorAll(".section__title"),
+    serviceTitles = document.querySelectorAll(".service__title"),
+    subTitle = document.querySelector(".sub__title"),
+    navMenu = document.querySelector(".header__nav__menu"),
+    navMenuButton = document.getElementById("nav__toggle"),
+    navButton = document.querySelector(".nav__btn"),
+    tabs = document.querySelectorAll(".header__nav__menu__link"),
+    tabsContent = document.querySelectorAll(".section"),
+    serviceTabs = document.querySelectorAll(".service__link"),
+    servicesTabsContent = document.querySelectorAll(".service"),
+    servicesTabParent = document.querySelector(".services__list"),
+    contactsServiceTabs = document.querySelectorAll(".contacts__service__link"),
+    contactsServicesTabParent = document.querySelector(".contacts__services__list"),
+    upButton = document.querySelector(".up__button"),
+    modal = document.querySelector(".modal__container"),
+    modalTrigger = document.querySelectorAll("[data-modal]"),
+    modalClosebtn = document.querySelector("[data-close]");
 
+  function hideServiceTitles() {
+    serviceTitles.forEach((item, i) => {
+      if (i % 2 == 0 || i === 0) {
+        item.classList.add("right__slide");
+      } else {
+        item.classList.add("left__slide");
+      }
+    });
+  }
 
+  function showNavMenu() {
+    navMenu.classList.remove("header__nav__menu");
+    navMenu.classList.add("header__nav__menu__visible");
+    navButton.classList.add("nav__btn__transform");
+  }
 
-        function hideServiceTitles(){
-            serviceTitles.forEach((item,i)=>{
-                if(i%2==0 || i===0){
-                    item.classList.add('right__slide');
-                }
-                else{
-                    item.classList.add('left__slide');
-                }
-            });
-        }
-        
-       
+  function hideNavMenu() {
+    navMenu.classList.add("header__nav__menu");
+    navMenu.classList.remove("header__nav__menu__visible");
+    navButton.classList.remove("nav__btn__transform");
+  }
 
-        function showNavMenu(){
-            navMenu.classList.remove('header__nav__menu');
-            navMenu.classList.add('header__nav__menu__visible');
-                navButton.classList.add('nav__btn__transform');
-            
+  navMenuButton.addEventListener("click", () => {
+    if (navButton.classList.contains("nav__btn__transform")) {
+      hideNavMenu();
+    } else {
+      showNavMenu();
     }
+  });
 
-    function hideNavMenu(){
-        navMenu.classList.add('header__nav__menu');
-        navMenu.classList.remove('header__nav__menu__visible'); 
-        navButton.classList.remove('nav__btn__transform');
+  upButton.classList.add("hide");
+
+  setTimeout(() => {
+    title.classList.remove("top__slide");
+    subTitle.classList.remove("hide");
+    subTitle.classList.add("show", "fade");
+  }, 1000);
+
+  function hideTabContent() {
+    tabsContent.forEach((item) => {
+      item.classList.add("hide");
+      item.classList.remove("show", "fade");
+    });
+    tabs.forEach((item) => {
+      item.classList.remove("active");
+    });
+  }
+
+  function showTabContent(i = 0) {
+    tabsContent[i].classList.add("show", "fade");
+    tabsContent[i].classList.remove("hide");
+    tabs[i].classList.add("active");
+
+    if (sectionTitles[i].classList.contains("right__slide")) {
+      setTimeout(() => {
+        sectionTitles[i].classList.remove("right__slide");
+      }, 1000);
+    } else if (sectionTitles[i].classList.contains("left__slide")) {
+      setTimeout(() => {
+        sectionTitles[i].classList.remove("left__slide");
+      }, 1000);
     }
+  }
 
-    navMenuButton.addEventListener('click',()=>{
-        if(navButton.classList.contains('nav__btn__transform')){
-            hideNavMenu();
-        }
-        else{
-            showNavMenu();
-        }
+  hideTabContent();
+  showTabContent();
+
+  function hideServiceTabContent() {
+    servicesTabsContent.forEach((item) => {
+      item.classList.add("hide");
+      item.classList.remove("show", "fade");
     });
 
+    serviceTabs.forEach((item) => {
+      item.parentElement.classList.remove("active__service");
+    });
+  }
 
-    
-    upButton.classList.add('hide');
+  function showServiceTabContent(j = 0) {
+    servicesTabsContent[j].classList.add("show", "fade");
+    servicesTabsContent[j].classList.remove("hide");
+    serviceTabs[j].parentElement.classList.add("active__service");
+  }
 
-    setTimeout(() => {
-        title.classList.remove('top__slide');
-        subTitle.classList.remove('hide');
-        subTitle.classList.add('show', 'fade'); 
-    }, 1000);
+  hideServiceTabContent();
 
+  navMenu.addEventListener("click", (event) => {
+    let target = event.target;
+    if (target && target.classList.contains("header__nav__menu__link")) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          window.scrollTo(0, 0);
+          hideTabContent();
+          showTabContent(i);
 
-
-
-    function hideTabContent() {
-        tabsContent.forEach(item => {
-            item.classList.add('hide');
-            item.classList.remove('show', 'fade');
-        });
-        tabs.forEach(item => {
-            item.classList.remove('active');
-        });
-    }
-
-    function showTabContent(i = 0) {
-        tabsContent[i].classList.add('show', 'fade');
-        tabsContent[i].classList.remove('hide');
-        tabs[i].classList.add('active');
-
-         if (sectionTitles[i].classList.contains('right__slide')) {
-            setTimeout(() => {
-                sectionTitles[i].classList.remove('right__slide');
-            }, 1000);
-        } else if (sectionTitles[i].classList.contains('left__slide')) {
-            setTimeout(() => {
-                sectionTitles[i].classList.remove('left__slide');
-            }, 1000);
-        } 
-    }
-
-    hideTabContent();
-    showTabContent();
-
-     function hideServiceTabContent() {
-        servicesTabsContent.forEach(item => {
-            item.classList.add('hide');
-            item.classList.remove('show', 'fade');
-        });
-        
-        serviceTabs.forEach(item => {
-            item.parentElement.classList.remove('active__service');
-        });
-    }
-
-    function showServiceTabContent(j = 0) {
-        servicesTabsContent[j].classList.add('show', 'fade');
-        servicesTabsContent[j].classList.remove('hide');
-        serviceTabs[j].parentElement.classList.add('active__service');
-    }
-
-    hideServiceTabContent(); 
-
-    navMenu.addEventListener('click', (event) => {
-        let target = event.target;
-        if (target && target.classList.contains('header__nav__menu__link')) {
-            tabs.forEach((item, i) => {
+          contactsServicesTabParent.addEventListener("click", (event) => {
+            let target = event.target;
+            if (
+              target &&
+              target.classList.contains("contacts__service__link")
+            ) {
+              hideTabContent();
+              showTabContent(1);
+              contactsServiceTabs.forEach((item, j) => {
                 if (target == item) {
-                    window.scrollTo(0, 0);
-                    hideTabContent();
-                    showTabContent(i);
-                    if(document.documentElement.clientWidth< 900){
-                        hideNavMenu();
+                  hideServiceTabContent();
+                  showServiceTabContent(j);
+
+                  serviceTitles.forEach((item) => {
+                    if (serviceTitles[j].classList.contains("right__slide")) {
+                      setTimeout(() => {
+                        serviceTitles[j].classList.remove("right__slide");
+                      }, 1000);
+                    } else if (
+                      serviceTitles[j].classList.contains("left__slide")
+                    ) {
+                      setTimeout(() => {
+                        serviceTitles[j].classList.remove("left__slide");
+                      }, 1000);
                     }
-                    
-                     if (i === 1) {
+                  });
+                }
+              });
+            }
+          });
 
-                        hideServiceTitles();
+          if (document.documentElement.clientWidth < 900) {
+            hideNavMenu();
+          }
+
+          if (i === 1) {
+            showServiceTabContent(0);
+            setTimeout(() => {
+              serviceTitles[0].classList.remove("right__slide");
+            }, 1000);
+            servicesTabParent.addEventListener("click", (event) => {
+              let target = event.target;
+              if (target && target.classList.contains("service__link")) {
+                serviceTabs.forEach((item, j) => {
+                  if (target == item) {
+                    hideServiceTabContent();
+                    showServiceTabContent(j);
+
+                    serviceTitles.forEach((item) => {
+                      if (serviceTitles[j].classList.contains("right__slide")) {
                         setTimeout(() => {
-                            serviceTitles[0].classList.remove('right__slide');
+                          serviceTitles[j].classList.remove("right__slide");
                         }, 1000);
-                        showServiceTabContent();
-                     
-                        servicesTabParent.addEventListener('click', (event) => {
-                            let target = event.target;
-                            if (target && target.classList.contains('service__button')) {
-                                
-                                serviceTabs.forEach((item, j) => {
-                                    if (target == item) {
-                                        hideServiceTabContent();
-                                        showServiceTabContent(j);
+                      } else if (
+                        serviceTitles[j].classList.contains("left__slide")
+                      ) {
+                        setTimeout(() => {
+                          serviceTitles[j].classList.remove("left__slide");
+                        }, 1000);
+                      }
+                    });
+                  }
+                });
+              }
+            });
+          }
+        }
+      });
+    }
+  });
 
-                                        serviceTitles.forEach(item => {
-                                            if (serviceTitles[j].classList.contains('right__slide')) {
-                                                setTimeout(() => {
-                                                    serviceTitles[j].classList.remove('right__slide');
-                                                }, 1000);
-                                            } else if (serviceTitles[j].classList.contains('left__slide')) {
-                                                setTimeout(() => {
-                                                    serviceTitles[j].classList.remove('left__slide');
-                                                }, 1000);
-                                            }
-                                        });
+  /* contactsServicesTabParent.addEventListener('click', (event) => {
+        let target = event.target;
+        if (target && target.classList.contains('contacts__service__link')) {
+            
+            serviceTabs.forEach((item, j) => {
+                if (target == item) {
+                    hideServiceTabContent();
+                    showServiceTabContent(j);
 
-                                    }
-                                });
-                            }
-                        });
-                    } 
+                    serviceTitles.forEach(item => {
+                        if (serviceTitles[j].classList.contains('right__slide')) {
+                            setTimeout(() => {
+                                serviceTitles[j].classList.remove('right__slide');
+                            }, 1000);
+                        } else if (serviceTitles[j].classList.contains('left__slide')) {
+                            setTimeout(() => {
+                                serviceTitles[j].classList.remove('left__slide');
+                            }, 1000);
+                        }
+                    });
+
                 }
             });
         }
-    });
+    }); */
 
+  //UPBUTTON
 
-    //UPBUTTON
+  window.onscroll = function () {
+    if (window.pageYOffset > 300) {
+      subTitle.classList.remove("show", "fade");
+      subTitle.classList.add("hide");
+      servicesTabParent.classList.add("to__the__top");
+      title.classList.add("top__slide");
+      header.classList.add("top__slide");
+      upButton.classList.remove("hide");
+      upButton.classList.add("show", "fade");
+    } else {
+      subTitle.classList.add("show", "fade");
+      subTitle.classList.remove("hide");
+      servicesTabParent.classList.remove("to__the__top");
 
-    window.onscroll = function () {
-        if (window.pageYOffset > 300) {
-            subTitle.classList.remove('show', 'fade');
-            subTitle.classList.add('hide');
-            servicesTabParent.classList.add('to__the__top');
-            title.classList.add('top__slide');
-            header.classList.add('top__slide');
-            upButton.classList.remove('hide');
-            upButton.classList.add('show', 'fade');
-        } else {
-            subTitle.classList.add('show', 'fade');
-            subTitle.classList.remove('hide');
-            servicesTabParent.classList.remove('to__the__top');
+      title.classList.remove("top__slide");
+      header.classList.remove("top__slide");
+      upButton.classList.add("hide");
+      upButton.classList.remove("show", "fade");
+    }
+  };
 
-            title.classList.remove('top__slide');
-            header.classList.remove('top__slide');
-            upButton.classList.add('hide');
-            upButton.classList.remove('show', 'fade');
-        }
-    };
+  upButton.addEventListener("click", () => {
+    window.scrollTo(0, 0);
+  });
 
-    upButton.addEventListener('click', () => {
-        window.scrollTo(0, 0);
-    });
-
-/* 
+  /* 
 
     //MODAL
 
@@ -244,6 +287,4 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     window.addEventListener('scroll', showModalByScroll); */
-
-
 });
